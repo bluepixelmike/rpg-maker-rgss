f5ac258c110d20794f17677aff019a54ec41f058
Michael Miller <bluepixelmike@gmail.com>
2015-11-25 01:07:11 -0700

    Add documentation for Rect class

--- lib/rect.rb
+++ lib/rect.rb
@@ -1,23 +1,56 @@
+# Rectangle class.
+# Contains two-dimensional position and size.
 class Rect
 
-  def initialize(x = 0, y = 0, width = 0, height = 0)
+  # Creates the rectangle.
+  # @overload initialize(x, y, width, height)
+  #   Creates a rectangle with predefined values.
+  #   @param x [Fixnum] X-coordinate of the rectangle's upper left corner.
+  #   @param y [Fixnum] Y-coordinate of the rectangle's upper left corner.
+  #   @param width [Fixnum] Width of the rectangle in pixels.
+  #   @param height [Fixnum] Height of the rectangle in pixels.
+  # @overload initialize()
+  #   Creates an empty rectangle.
+  #   The position will be set to (0, 0) and the size set to 0x0.
+  def initialize
     fail NotImplementedError
   end
 
-  def set(arg1, y = nil, width = nil, height = nil)
+  # Updates all of the components at once.
+  # @overload set(x, y, width, height)
+  #   Sets all components.
+  #   @param x [Fixnum] New x-coordinate of the rectangle's upper left corner.
+  #   @param y [Fixnum] New y-coordinate of the rectangle's upper left corner.
+  #   @param width [Fixnum] New width of the rectangle in pixels.
+  #   @param height [Fixnum] New height of the rectangle in pixels.
+  # @overload set(rect)
+  #   Copies the components from another rectangle.
+  #   @param rect [Rect] Rectangle to copy components from.
+  # @return [self]
+  def set(*args)
     fail NotImplementedError
   end
 
+  # Sets all components to zero.
+  # @return [self]
   def empty
     fail NotImplementedError
   end
 
+  # X-coordinate of the rectangle's upper left corner.
+  # @return [Fixnum]
   attr_accessor :x
 
+  # Y-coordinate of the rectangle's upper left corner.
+  # @return [Fixnum]
   attr_accessor :y
 
+  # Width of the rectangle in pixels.
+  # @return [Fixnum]
   attr_accessor :width
 
+  # Height of the rectangle in pixels.
+  # @return [Fixnum]
   attr_accessor :height
 
 end
